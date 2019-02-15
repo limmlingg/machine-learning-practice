98c03e57704f4c90397f6a7cf88465023cb014c9
Miao Ling <limmlingg@gmail.com>
2019-02-15 17:50:39 +0800

    Add files via upload

--- iris flower classification 3.py
+++ iris flower classification 3.py
@@ -21,4 +21,47 @@ dataset['species'].replace(0, 'Iris-setosa',inplace=True)
 dataset['species'].replace(1, 'Iris-versicolor',inplace=True)
 dataset['species'].replace(2, 'Iris-virginica',inplace=True)
 
-print(dataset.head(5))
+# print(dataset.head(5))
+
+import matplotlib.pyplot as plt
+
+# plt.figure(4, figsize=(10, 8))
+
+# plt.scatter(data[:50, 0], data[:50, 1], c='r', label='Iris-setosa')
+
+# plt.scatter(data[50:100, 0], data[50:100, 1], c='g',label='Iris-versicolor')
+
+# plt.scatter(data[100:, 0], data[100:, 1], c='b',label='Iris-virginica')
+
+# plt.xlabel('Sepal length',fontsize=20)
+# plt.ylabel('Sepal width',fontsize=20)
+# plt.xticks(fontsize=20)
+# plt.yticks(fontsize=20)
+# plt.title('Sepal length vs. Sepal width',fontsize=20)
+# plt.legend(prop={'size': 18})
+# # plt.show()
+
+# plt.figure(4, figsize=(8, 8))
+
+# plt.scatter(data[:50, 2], data[:50, 3], c='r', label='Iris-setosa')
+
+# plt.scatter(data[50:100, 2], data[50:100, 3], c='g',label='Iris-versicolor')
+
+# plt.scatter(data[100:, 2], data[100:, 3], c='b',label='Iris-virginica')
+# plt.xlabel('Petal length',fontsize=15)
+# plt.ylabel('Petal width',fontsize=15)
+# plt.xticks(fontsize=15)
+# plt.yticks(fontsize=15)
+# plt.title('Petal length vs. Petal width',fontsize=15)
+# plt.legend(prop={'size': 20})
+# # plt.show()
+
+print(dataset.iloc[:,2:].corr())
+print(dataset.iloc[:50,:].corr()) #setosa
+print(dataset.iloc[50:100,:].corr()) #versicolor
+print(dataset.iloc[100:,:].corr()) #virginica
+
+fig = plt.figure(figsize = (8,8))
+ax = fig.gca()
+dataset.hist(ax=ax)
+plt.show()
